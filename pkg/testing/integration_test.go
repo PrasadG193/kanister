57c3b3f607b051114cb9487917ebbfe88268a445
Prasad Ghangal <prasad.ghangal@gmail.com>
2019-11-07 13:05:37 +0530

    Refactor codebase to support new e2e test interface
    Signed-off-by: Prasad Ghangal <prasad.ghangal@gmail.com>

--- pkg/testing/integration_test.go
+++ pkg/testing/integration_test.go
@@ -22,9 +22,11 @@ import (
 	log "github.com/sirupsen/logrus"
 	. "gopkg.in/check.v1"
 	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 
 	crv1alpha1 "github.com/kanisterio/kanister/pkg/apis/cr/v1alpha1"
+	"github.com/kanisterio/kanister/pkg/app"
 	crclient "github.com/kanisterio/kanister/pkg/client/clientset/versioned/typed/cr/v1alpha1"
 	"github.com/kanisterio/kanister/pkg/controller"
 	_ "github.com/kanisterio/kanister/pkg/function"
@@ -32,29 +34,26 @@ import (
 	"github.com/kanisterio/kanister/pkg/kube"
 	"github.com/kanisterio/kanister/pkg/poll"
 	"github.com/kanisterio/kanister/pkg/resource"
-	"github.com/kanisterio/kanister/pkg/testing/app"
-	"github.com/kanisterio/kanister/pkg/testing/db"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"github.com/kanisterio/kanister/pkg/testutil"
 )
 
-type test struct {
-	database    db.Instance
-	application app.Kanister
-	namespace   string
-	skip        bool
-}
-
 type IntegrationSuite struct {
-	cli          kubernetes.Interface
-	crCli        crclient.CrV1alpha1Interface
-	tests        map[string]*test
-	cancel       context.CancelFunc
-	controllerNs string
+	name      string
+	cli       kubernetes.Interface
+	crCli     crclient.CrV1alpha1Interface
+	app       app.App
+	bp        app.Blueprinter
+	namespace string
+	skip      bool
+	cancel    context.CancelFunc
 }
 
+// Add app test suites
 var _ = Suite(&IntegrationSuite{
-	tests:        make(map[string]*test),
-	controllerNs: "e2e-test",
+	name:      "rds-postgres",
+	namespace: "rds-postgres-test",
+	app:       app.NewPostgresDB(),
+	bp:        app.NewPostgresBP(),
 })
 
 func (s *IntegrationSuite) SetUpSuite(c *C) {
@@ -69,117 +68,167 @@ func (s *IntegrationSuite) SetUpSuite(c *C) {
 	s.crCli, err = crclient.NewForConfig(cfg)
 	c.Assert(err, IsNil)
 
-	postgresDB, err := db.NewPostgresDB(s.cli)
-	c.Assert(err, IsNil)
-
-	// Add new tests in the map
-	s.tests = map[string]*test{
-		"rds-postgres": &test{
-			database:    db.Instance{Type: db.ManagedDB, Database: postgresDB},
-			application: app.NewPostgresApp(s.cli, s.crCli, "e2e-rds-postgres"),
-			namespace:   "e2e-rds-postgres",
-			skip:        false,
-		},
-	}
-
-	// Create a new test namespace
-	ns := &v1.Namespace{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: s.controllerNs,
-		},
-	}
-	_, err = s.cli.CoreV1().Namespaces().Create(ns)
-	c.Assert(err, IsNil)
-
 	// Start the controller
 	err = resource.CreateCustomResources(ctx, cfg)
 	c.Assert(err, IsNil)
 	ctlr := controller.New(cfg)
-	err = ctlr.StartWatch(ctx, s.controllerNs)
+	err = ctlr.StartWatch(ctx, s.namespace)
 	c.Assert(err, IsNil)
 }
 
+// TestRun executes e2e workflow on the app
+// 1. Install DB app
+// 2. Add data
+// 3. Create Kanister Profile and Blueprint
+// 4. Take Backup
+// 5. Delete DB data
+// 6. Restore data from backup
+// 7. Uninstall DB app
 func (s *IntegrationSuite) TestRun(c *C) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	// Exec methods
-	for name, t := range s.tests {
-		log.Infof("Running e2e integration test for %s", name)
-		// Check config
-		err := t.database.GetConfig(ctx)
-		if err != nil {
-			log.Infof("Skipping integration test for %s. Reason: %s", name, err.Error())
-			s.tests[name].skip = true
-			continue
-		}
+	// Execute e2e workflow
+	log.Infof("Running e2e integration test for %s", s.name)
+	// Check config
+	err := s.app.Init(ctx)
+	if err != nil {
+		log.Infof("Skipping integration test for %s. Reason: %s", s.name, err.Error())
+		s.skip = true
+		c.Skip(err.Error())
+	}
 
-		// Install db
-		err = t.database.Install(ctx, t.namespace)
+	err = createNamespace(s.cli, s.namespace)
+	c.Assert(err, IsNil)
+
+	// Install db
+	err = s.app.Install(ctx, s.namespace)
+	c.Assert(err, IsNil)
+
+	// Check if ready
+	ok, err := s.app.IsReady(ctx)
+	c.Assert(err, IsNil)
+	c.Assert(ok, Equals, true)
+
+	// Create profile
+	_, location := testutil.ObjectstoreLocationOrSkip()
+	secret, profile, err := testutil.NewSecretProfile(c, s.name, s.namespace, location)
+	c.Assert(err, IsNil)
+
+	_, err = s.cli.CoreV1().Secrets(s.namespace).Create(secret)
+	c.Assert(err, IsNil)
+	_, err = s.crCli.Profiles(s.namespace).Create(profile)
+	c.Assert(err, IsNil)
+
+	// Create blueprint
+	bp := s.bp.Blueprint()
+	_, err = s.crCli.Blueprints(s.namespace).Create(bp)
+	c.Assert(err, IsNil)
+
+	var configMaps, secrets map[string]crv1alpha1.ObjectReference
+	testEntries := 3
+	// Add test entries to DB
+	if a, ok := s.app.(app.DatabaseApp); ok {
+		err = a.Ping(ctx)
 		c.Assert(err, IsNil)
 
-		// Create configmap and secret for in case of Managed DB
-		if t.database.Type == db.ManagedDB {
-			err := t.database.CreateConfig(ctx, t.namespace)
-			c.Assert(err, IsNil)
-		}
-
-		// Install Application
-		err = t.application.Install(ctx, s.controllerNs)
-		c.Assert(err, IsNil)
-
-		// Check connection
-		data := t.database.Data()
-		err = data.Ping(ctx)
-		c.Assert(err, IsNil)
-
-		err = data.Reset(ctx)
+		err = a.Reset(ctx)
 		c.Assert(err, IsNil)
 
 		// Add few entries
-		err = data.Insert(ctx, 3)
+		err = a.Insert(ctx, testEntries)
 		c.Assert(err, IsNil)
 
-		count, err := data.Count(ctx)
+		count, err := a.Count(ctx)
 		c.Assert(err, IsNil)
-		c.Assert(count, Equals, 3)
-
-		// Take backup
-		backup := s.createActionset(ctx, t.application.ActionSet(), t.namespace, "backup", c)
-
-		// Reset DB
-		err = data.Reset(ctx)
-		c.Assert(err, IsNil)
-
-		// Restore backup
-		c.Assert(len(backup), Not(Equals), 0)
-		pas, err := s.crCli.ActionSets(s.controllerNs).Get(backup, metav1.GetOptions{})
-		c.Assert(err, IsNil)
-		s.createActionset(ctx, pas, t.namespace, "restore", c)
-
-		// Verify data
-		err = data.Ping(ctx)
-		c.Assert(err, IsNil)
-
-		count, err = data.Count(ctx)
-		c.Assert(err, IsNil)
-		c.Assert(count, Equals, 3)
-
-		// Delete snapshots
-		s.createActionset(ctx, pas, t.namespace, "delete", c)
+		c.Assert(count, Equals, testEntries)
 	}
+
+	// Get Secret and ConfigMap object references
+	if a, ok := s.app.(app.ConfigApp); ok {
+		configMaps = a.ConfigMaps()
+		secrets = a.Secrets()
+	}
+
+	// Create ActionSet specs
+	as := newActionSet(bp.GetName(), profile.GetName(), s.namespace, s.app.Object(), configMaps, secrets)
+
+	// Take backup
+	backup := s.createActionset(ctx, c, as, "backup")
+	c.Assert(len(backup), Not(Equals), 0)
+
+	// Reset DB
+	if a, ok := s.app.(app.DatabaseApp); ok {
+		err = a.Reset(ctx)
+		c.Assert(err, IsNil)
+
+		count, err := a.Count(ctx)
+		c.Assert(err, IsNil)
+		c.Assert(count, Equals, 0)
+	}
+
+	// Restore backup
+	pas, err := s.crCli.ActionSets(s.namespace).Get(backup, metav1.GetOptions{})
+	c.Assert(err, IsNil)
+	s.createActionset(ctx, c, pas, "restore")
+
+	// Verify data
+	if a, ok := s.app.(app.DatabaseApp); ok {
+		err = a.Ping(ctx)
+		c.Assert(err, IsNil)
+
+		count, err := a.Count(ctx)
+		c.Assert(err, IsNil)
+		c.Assert(count, Equals, testEntries)
+	}
+
+	// Delete snapshots
+	s.createActionset(ctx, c, pas, "delete")
 }
 
-func (s *IntegrationSuite) createActionset(ctx context.Context, as *crv1alpha1.ActionSet, appNs, action string, c *C) string {
+func newActionSet(bpName, profile, profileNs string, object crv1alpha1.ObjectReference, configMaps, secrets map[string]crv1alpha1.ObjectReference) *crv1alpha1.ActionSet {
+	return &crv1alpha1.ActionSet{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: "test-actionset-",
+		},
+		Spec: &crv1alpha1.ActionSetSpec{
+			Actions: []crv1alpha1.ActionSpec{
+				crv1alpha1.ActionSpec{
+					Name:      "backup",
+					Object:    object,
+					Blueprint: bpName,
+					Profile: &crv1alpha1.ObjectReference{
+						Name:      profile,
+						Namespace: profileNs,
+					},
+					ConfigMaps: configMaps,
+					Secrets:    secrets,
+					PodOverride: map[string]interface{}{
+						"containers": []map[string]interface{}{
+							{
+								"name":            "container",
+								"imagePullPolicy": "IfNotPresent",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+}
+
+// createActionset creates and wait for actionset to complete
+func (s *IntegrationSuite) createActionset(ctx context.Context, c *C, as *crv1alpha1.ActionSet, action string) string {
 	var err error
 	switch action {
 	case "backup":
-		as, err = s.crCli.ActionSets(s.controllerNs).Create(as)
+		as, err = s.crCli.ActionSets(s.namespace).Create(as)
 		c.Assert(err, IsNil)
 	case "restore", "delete":
 		as, err = restoreActionSetSpecs(as, action)
 		c.Assert(err, IsNil)
-		as, err = s.crCli.ActionSets(s.controllerNs).Create(as)
+		as, err = s.crCli.ActionSets(s.namespace).Create(as)
 		c.Assert(err, IsNil)
 	default:
 		c.Errorf("Invalid action %s while creating ActionSet", action)
@@ -187,7 +236,7 @@ func (s *IntegrationSuite) createActionset(ctx context.Context, as *crv1alpha1.A
 
 	// Wait for the ActionSet to complete.
 	err = poll.Wait(ctx, func(ctx context.Context) (bool, error) {
-		as, err = s.crCli.ActionSets(s.controllerNs).Get(as.GetName(), metav1.GetOptions{})
+		as, err = s.crCli.ActionSets(s.namespace).Get(as.GetName(), metav1.GetOptions{})
 		switch {
 		case err != nil, as.Status == nil:
 			return false, err
@@ -202,6 +251,7 @@ func (s *IntegrationSuite) createActionset(ctx context.Context, as *crv1alpha1.A
 	return as.GetName()
 }
 
+// restoreActionSetSpecs generates restore actionset specs from backup name
 func restoreActionSetSpecs(from *crv1alpha1.ActionSet, action string) (*crv1alpha1.ActionSet, error) {
 	params := kanctl.PerformParams{
 		ActionName: action,
@@ -210,23 +260,32 @@ func restoreActionSetSpecs(from *crv1alpha1.ActionSet, action string) (*crv1alph
 	return kanctl.ChildActionSet(from, &params)
 }
 
+func createNamespace(cli kubernetes.Interface, name string) error {
+	// Create Namespace
+	ns := &v1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+	}
+	_, err := cli.CoreV1().Namespaces().Create(ns)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func (s *IntegrationSuite) TearDownSuite(c *C) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	for name, t := range s.tests {
-		if !t.skip {
-			log.Infof("Deleting DB for %s", name)
-			t.database.Remove(ctx, t.namespace)
-			t.application.Remove(ctx, t.namespace)
-		}
-		if len(t.namespace) != 0 {
-			s.cli.CoreV1().Namespaces().Delete(t.namespace, nil)
-		}
+	// Uninstall app
+	if !s.skip {
+		err := s.app.Uninstall(ctx)
+		c.Assert(err, IsNil)
 	}
 
-	// Delete e2e-test ns
-	s.cli.CoreV1().Namespaces().Delete(s.controllerNs, nil)
+	// Delete namespace
+	s.cli.CoreV1().Namespaces().Delete(s.namespace, nil)
 	if s.cancel != nil {
 		s.cancel()
 	}
